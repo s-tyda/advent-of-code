c024de67171357551e8231168956090d44fe44fe
s-tyda <Sebastian.Tyda@Live.umcs.edu.pl>
2020-12-09 17:31:40 +0100

    Edited format in Day 6 python solution

--- Day_06/day6.py
+++ Day_06/day6.py
@@ -1,4 +1,4 @@
 # Part 1
-print(sum(len(set(i.replace('\n', ''))) for i in open('data.txt').read().split('\n\n')))
+print("Part 1: {}".format(sum(len(set(i.replace('\n', ''))) for i in open('data.txt').read().split('\n\n'))))
 # Part 2
-print(sum(len(set.intersection(*[set(t) for t in i.split("\n")])) for i in open('data.txt').read().split("\n\n")))
+print("Part 2: {}".format(sum(len(set.intersection(*[set(t) for t in i.split("\n")])) for i in open('data.txt').read().split("\n\n"))))
