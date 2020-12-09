693687bcc23b5ace8e93422eab955085ba456a1b
s-tyda <Sebastian.Tyda@Live.umcs.edu.pl>
2020-12-09 18:25:42 +0100

    Changed Day 7 - python solution - to oneliner

--- Day_07/day7.py
+++ Day_07/day7.py
@@ -1,10 +1,5 @@
 from parse import *
-x = {search('{} bag', x)[0]: [*findall('{:d} {} bag', x)] for x in open('data.txt')}
-
-
-def f(c): return any(d == 'shiny gold' or f(d) for _, d in x[c])
-def g(c): return sum(n + n * g(d) for n, d in x[c])
-
-
-print(f"Part 1: {sum(map(f, x))}")
-print(f"Part 2: {g('shiny gold')}")
+# Part 1
+print(f"Part 1: {sum(map(f := lambda c: any(d == 'shiny gold' or f(d) for _, d in x[c]), x := {search('{} bag', x)[0]: [*findall('{:d} {} bag', x)] for x in open('data.txt')}))}")
+# Part 2
+print(f"Part 2: {(g := lambda c: sum(n + n * g(d) for n, d in x[c]))('shiny gold')}")
