539f96a9591a233d8c7277c9dce5dfb5f706eaef
s-tyda <Sebastian.Tyda@Live.umcs.edu.pl>
2020-12-09 17:09:21 +0100

    Added Day 4 - Part 2 - python regex solution

--- Day_04/day4.py
+++ Day_04/day4.py
@@ -18,3 +18,14 @@ print("Part 2: {}".format(sum(
                 ({j.split(':')[0]: j.split(':')[1] for j in re.split('\n| ', x) if ':' in j})
        else 0 for x in open('data.txt').read().split("\n\n")]
 )))
+
+# Part 2 - regex
+print("Part 2: {}".format(sum(all(re.search(i, x) for i in [
+    r"byr:(19[2-9]\d|200[0-2])",
+    r"iyr:20(1\d|20)",
+    r"eyr:20(2\d|30)",
+    r"hgt:(1([5-8]\d|9[0-3])cm|(59|6\d|7[0-6])in)",
+    r"hcl:#[0-9a-f]{6}",
+    r"ecl:(amb|blu|brn|gry|grn|hzl|oth)",
+    r"pid:\d{9}\b"
+])for x in open("data.txt").read().split("\n\n"))))
